f8cefd49be5902c204a97c452b62ea643b4f999b
Fallito <Fallitopotron@gmail.com>
2023-04-14 07:14:54 -0400

    add new icons

--- ficons.sh
+++ ficons.sh
@@ -4,14 +4,15 @@
 # * @file git.sh
 # * @push guide.xml
 # * @author Fallito
-# * @date 14/feb/2021
-# * PASS=Giovanni7feb2021@github.com
+# * @date  29/mar/2023
+# * USER=Fallito
+# * PASS=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
 # */
 
 #push ./guide.xml for daily cronjob
 
 USER=Fallito
-PASS=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
+PASS=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
 
 echo "status"
 
